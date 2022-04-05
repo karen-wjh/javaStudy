de6780a86772f7cefb938df924adbb63fe9c3b1d
karen-wjh <wangjinghan0725@icloud.com>
2022-04-05 11:51:38 +0900

    第一次提交

---
+++ firstJava/src/com/wjh/co/jp/testproject/firstTest.java
@@ -0,0 +1,8 @@
+package com.wjh.co.jp.testproject;
+
+
+public class firstTest {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
