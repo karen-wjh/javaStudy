d3683a08ff5735c227dbb90ed9eb2ee8ea6b241d
karen-wjh <wangjinghan0725@icloud.com>
2022-04-06 10:51:04 +0900

    commit

--- firstJava/src/test3.java
+++ firstJava/src/test3.java
@@ -1,8 +1,17 @@
 public class test3 {
     public static void main(String[] args){
-        short i =(120+34)*8;
+      
+      
+      /*  short i =(120+34)*8;
         int x =7 * 18;
         System.out.println(x);
         System.out.println(i);
     }
+}*/
+//四舍五入
+
+double d = 2.6;
+int n = (int) (d + 0.5);
+System.out.println(n);
+}
 }
