9ed81758796585a4f94dba3126171f90b772da92
karen-wjh <wangjinghan0725@icloud.com>
2022-04-07 18:36:54 +0900

    main

--- firstJava/src/com/wjh/co/jp/testproject/hhh.java
+++ firstJava/src/com/wjh/co/jp/testproject/hhh.java
@@ -1,7 +1,8 @@
 package com.wjh.co.jp.testproject;
 
 public class hhh{
-    public static void main(String[] args){
+  
+  /*  public static void main(String[] args){
         int n = 3300;
         int y =30;
         double z =336 % 78;
@@ -9,3 +10,22 @@ public class hhh{
         System.out.println(m);
       }
 }
+*/
+
+public static void main(String[] args) {
+    int a = 65;
+    int b = 66;
+    int c = 67;
+  //  String s ="72"+" "+"105"+" " +"65281";
+// FIXME:
+    String str1 = "\u0065\u0066\u0067";
+    StringBuffer str2 = new StringBuffer();
+    str2.append((char)a);
+    str2.append((char)b);
+    str2.append((char)c);
+    //String s = a + b + c;
+    System.out.println(str1);
+    System.out.println(str2);
+}
+}
+
---
+++ firstJava/src/com/wjh/co/jp/testproject/hw.java
@@ -0,0 +1,9 @@
+package com.wjh.co.jp.testproject;
+
+public class hw{
+    public static void main(String[] argw){
+        int a = 66;
+        char b = (char)a;
+        System.out.println(b);
+    }
+}
