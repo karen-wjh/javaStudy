522d2711c1ef5b6203839106093e1e8f5679a9d5
karen-wjh <wangjinghan0725@icloud.com>
2022-04-07 18:58:06 +0900

    main

--- firstJava/src/com/wjh/co/jp/testproject/hw.java
+++ firstJava/src/com/wjh/co/jp/testproject/hw.java
@@ -1,5 +1,5 @@
 package com.wjh.co.jp.testproject;
-
+/*
 public class hw{
     public static void main(String[] argw){
         int a = 66;
@@ -7,3 +7,21 @@ public class hw{
         System.out.println(b);
     }
 }
+*/
+
+public class hw{
+    public static void main(String[]args){
+        int a =72;
+        int b =105;
+        int c =65281;
+        char a1=(char)a;
+        char b1=(char)b;
+        char c1=(char)c;
+        String s =a1+" "+b1+" "+c1;
+        System.out.println(a1);
+        System.out.println(b1);
+        System.out.println(c1);
+        System.out.println(s);
+    }
+}
+
