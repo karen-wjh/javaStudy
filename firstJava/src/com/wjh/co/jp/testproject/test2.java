d623a4637ef982f6ca76c89bf08d59624b33f088
karen-wjh <wangjinghan0725@icloud.com>
2022-04-07 14:40:13 +0900

    main

--- firstJava/src/com/wjh/co/jp/testproject/test2.java
+++ firstJava/src/com/wjh/co/jp/testproject/test2.java
@@ -1,5 +1,9 @@
 package com.wjh.co.jp.testproject;
 
+import java.time.Year;
+
+import javax.net.ssl.SSLContext;
+
 public class test2 {
     /*
     public static void main(String[] args) {
@@ -26,9 +30,34 @@ public static void main(String[] args){
     }
         }
 */
+/*
 public static void main(String[] args){
     int i = 167776589; // 00001010 00000000 00010001 01001101
         int n = 167776512; // 00001010 00000000 00010001 00000000
         System.out.println(i & n); // 167776512
     }
 }
+*/
+
+
+/*
+public static void main(String[] args)
+{
+    int n = 100;
+
+    int x =762;
+
+    int y =n* x;
+    System.out.println(y);
+}
+          }
+
+          */
+
+
+          public static void main(String[] args){
+              String s1 = "Hello";
+              String s2 = "World";
+              String s = s1 + " " +s2 + "!";
+              System.out.println(s);
+          }
