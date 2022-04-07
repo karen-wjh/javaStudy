1a76dfb295b352bdb35d2d4e3f7a2940ebb2feed
karen-wjh <wangjinghan0725@icloud.com>
2022-04-07 14:40:22 +0900

    main

--- firstJava/src/com/wjh/co/jp/testproject/kadiwh.java
+++ firstJava/src/com/wjh/co/jp/testproject/kadiwh.java
@@ -1,7 +1,42 @@
-kadiwh
+package com.wjh.co.jp.testproject;
 
+import javax.xml.crypto.dsig.spec.C14NMethodParameterSpec;
 
+/*
+public class kadiwh {
+    public static void main(String[] args) {
+        double x = 1.0 / 10;
+        double y = 1 - 0.9 / 10;
+        //观察xy是否相等
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
+*/
+//求和公式例：
+/*
+public class kadiwh {
+    public static void main (String[] args){
+        int n =100;
+        int sum = (1+n)*n/2;
+        System.out.println(sum);
+    }
+}
+*/
+//求根公式
+public class kadiwh {
+    public static void main(String [] args){
+        double a = 1.0;
+        double b =3.0;
+        double c =-4.0;
+        double d =( b * b )-(4 * a * c);
+        double d1 =Math.sqrt(d);
+      
+        double e = ((-b+d1) / 2*a );
+    double f = ((-b-d1) / 2 * a );
+    System.out.println(d);
+System.out.println(e );
+System.out.println(f);
 
-gfyklll
-jjjj]
-yfyf
+    }
+}
