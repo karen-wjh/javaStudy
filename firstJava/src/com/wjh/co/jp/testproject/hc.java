19e763716a092165b18d64bff05f0be06e96a8d2
karen-wjh <wangjinghan0725@icloud.com>
2022-04-12 14:37:17 +0900

    hji

---
+++ firstJava/src/com/wjh/co/jp/testproject/hc.java
@@ -0,0 +1,34 @@
+package com.wjh.co.jp.testproject;
+
+import java.util.Scanner;
+//do while
+
+public class hc {
+    public static void main(String[] args) {
+            /*  
+        //do while
+        int sum = 0;
+                int m = 20;
+                do{
+                    sum += m;
+                    m ++;
+
+                }
+                // 使用while计算M+...+N:
+                while (m <=100);
+                
+                System.out.println(sum);
+
+                */
+
+                //for
+                int sum = 0;
+                for(int i=1; i<=100; i++){
+                    sum = sum + i;
+                }
+                System.out.println(sum);
+            }
+        }
+        
+    
+
