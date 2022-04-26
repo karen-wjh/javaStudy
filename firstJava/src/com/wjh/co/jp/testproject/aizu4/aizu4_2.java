9a573b167a4baeb8b21b92abe09ed66a31dd6d84
karen-wjh <wangjinghan0725@icloud.com>
2022-04-26 17:56:58 +0900

    shuzu

---
+++ firstJava/src/com/wjh/co/jp/testproject/aizu4/aizu4_2.java
@@ -0,0 +1,19 @@
+package com.wjh.co.jp.testproject.aizu4;
+//https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/4/ITP1_4_B
+
+
+import java.util.Scanner;
+
+public class aizu4_2 {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        double r = num.nextDouble();
+        double a = r*r*3.141592653589;
+        double b = 2*r*3.141592653589;
+        System.out.printf("%.6f",a);
+        System.out.print(" ");
+        System.out.printf("%.6f",b);
+
+    }
+    
+}
--- firstJava/src/com/wjh/co/jp/testproject/aizu4/aizu4_2java.java
+++
@@ -1,9 +0,0 @@
-package com.wjh.co.jp.testproject.aizu4;
-
-
-public class aizu4_2java {
-    public static void main(String[] args) {
-                
-    }
-    
-}
