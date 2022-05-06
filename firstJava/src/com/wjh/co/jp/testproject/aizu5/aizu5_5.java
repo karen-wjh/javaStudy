cb03e3091c3ca6f3be20546d7be5439e7bf2a125
karen-wjh <wangjinghan0725@icloud.com>
2022-05-06 16:59:38 +0900

    mia

---
+++ firstJava/src/com/wjh/co/jp/testproject/aizu5/aizu5_5.java
@@ -0,0 +1,32 @@
+/*
+https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/5/ITP1_5_D
+ */
+package com.wjh.co.jp.testproject.aizu5;
+import java.util.Scanner;
+public class aizu5_5 {
+    public static void main(String[] args) {
+        method();
+    }
+    public static void method() {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int i = 1;
+        while (i <= a) {
+            int x = i;
+            if (x % 3 == 0) {
+                System.out.print(" " + i);
+            } else {
+                while (x != 0) {
+                    if (x % 10 == 3) {
+                        System.out.print(" " + i);
+                        break;
+                    } else {
+                        x /= 10;
+                    }
+                }
+            }
+                    i++;
+        }
+                System.out.println();
+    }
+}
