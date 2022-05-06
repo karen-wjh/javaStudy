9f7cbac8ce191310c1cd787f69d3f77c5c13f2fe
karen-wjh <wangjinghan0725@icloud.com>
2022-05-06 10:58:36 +0900

    main

---
+++ firstJava/src/com/wjh/co/jp/testproject/aizu5/aizu5_4.java
@@ -0,0 +1,12 @@
+package com.wjh.co.jp.testproject.aizu5;
+
+import java.util.Scanner;
+
+public class aizu5_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int H = sc.nextInt();
+        int W = sc.nextInt();
+    
+    }
+}
