4481f6988ce5a0ccf08379edd1d41ad60a584605
karen-wjh <wangjinghan0725@icloud.com>
2022-05-07 22:18:52 +0900

    mij

---
+++ firstJava/src/com/wjh/co/jp/testproject/hw1.java
@@ -0,0 +1,9 @@
+package com.wjh.co.jp.testproject;
+
+public class hw1 {
+    public static void main(String[] args) {
+    System.out.println("hello,world");
+        
+    }
+
+}
--- firstJava/src/com/wjh/co/jp/testproject/test2.java
+++ firstJava/src/com/wjh/co/jp/testproject/test2.java
@@ -5,15 +5,14 @@ import java.time.Year;
 import javax.net.ssl.SSLContext;
 
 public class test2 {
-    /*
+    
     public static void main(String[] args) {
         int i = (100 + 200) * (99 - 88); // 3300
         int n = 7 * (5 + (i - 9)); // 23072
         System.out.println(i);
         System.out.println(n);
-}**/
 
-/*
+
 public static void main(String[] args){
     short i =(120+34)*8;
     int x =7 * 18;
@@ -21,8 +20,8 @@ public static void main(String[] args){
     System.out.println(i);
 }
     }
-    */
-/*
+    
+
     public static void main(String[] args){
         int i =100; 
         int m =(i + 1) * i/2;
@@ -30,7 +29,7 @@ public static void main(String[] args){
     }
         }
 */
-/*
+
 public static void main(String[] args){
     int i = 167776589; // 00001010 00000000 00010001 01001101
         int n = 167776512; // 00001010 00000000 00010001 00000000
@@ -40,7 +39,7 @@ public static void main(String[] args){
 */
 
 
-/*
+
 public static void main(String[] args)
 {
     int n = 100;
@@ -52,7 +51,7 @@ public static void main(String[] args)
 }
           }
 
-          */
+          
 
 
           public static void main(String[] args){
