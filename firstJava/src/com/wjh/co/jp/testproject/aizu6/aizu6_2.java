dd7ad612416048887aa189103450cff0c6782151
karen-wjh <wangjinghan0725@icloud.com>
2022-05-09 11:25:14 +0900

    mm

---
+++ firstJava/src/com/wjh/co/jp/testproject/aizu6/aizu6_2.java
@@ -0,0 +1,14 @@
+package com.wjh.co.jp.testproject.aizu6;
+
+import java.util.Scanner;
+
+public class aizu6_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] S =new int[13];
+        int[] H =new int[13];
+        int[] C =new int[13];
+        int[] D =new int[13];
+        
+    }
+}
