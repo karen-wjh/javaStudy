2ba91a201b88ad431d7dbf8599e76d7c52941389
karen-wjh <wangjinghan0725@icloud.com>
2022-05-11 14:46:50 +0900

    mai

--- firstJava/src/com/wjh/co/jp/testproject/aizu6/aizu6_4.java
+++ firstJava/src/com/wjh/co/jp/testproject/aizu6/aizu6_4.java
@@ -1,11 +1,31 @@
 package com.wjh.co.jp.testproject.aizu6;
-import java.util.Scanner;
-public class aizu6_4 {
-    public static void main(String[] args) {mmain(
-    );
-        Scanner sc = new Scanner(System.in);
-        int a = new.nextInt();
-        int b = new.nextInt();
 
+import java.util.Scanner;
+
+
+public class aizu6_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int [][] array = new int[a][b];
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+            array [i][j] = sc.nextInt();
+            }
+        }
+        int [] arrayA =new int [b];
+        for (int i = 0; i < b; i++) {
+            arrayA [i] =sc.nextInt(); 
+        }
+        int num;
+        for (int i = 0; i < a; i++) {
+            num =0;
+            for (int j = 0; j < b; j++) {
+                num += array[i][j] * arrayA[j];
+            }
+            System.out.println(num);
+        }
     }
 }
+
