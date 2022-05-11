aa26a7be8f3f42020a64a3ab43ddf7eb8c2ae24d
karen-wjh <wangjinghan0725@icloud.com>
2022-05-11 18:14:16 +0900

    mm

---
+++ firstJava/src/com/wjh/co/jp/testproject/aizu7/aizu7_2.java
@@ -0,0 +1,26 @@
+package com.wjh.co.jp.testproject.aizu7;
+
+import java.util.Scanner;
+
+public class aizu7_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true){
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+        if(n == 0 || x == 0){
+            return;
+        }
+        int count=0;
+        for (int i = 0; i < n-2; i++) {
+            for (int j = i+1; j <n-1 ; j++) {
+                for (int k = j+1; k < n; k++) {
+                    if( (i+j+k)==x){
+                        count++;
+                      }
+                   }
+                }
+             }System.out.println(count);
+         }  
+    }
+}
