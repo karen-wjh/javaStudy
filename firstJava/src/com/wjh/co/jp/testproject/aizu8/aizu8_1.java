cef2f78e7e2eda20715ed121a253a93d4ebd9280
karen-wjh <wangjinghan0725@icloud.com>
2022-05-16 18:08:55 +0900

    8.1

---
+++ firstJava/src/com/wjh/co/jp/testproject/aizu8/aizu8_1.java
@@ -0,0 +1,10 @@
+package com.wjh.co.jp.testproject.aizu8;
+
+import java.util.Scanner;
+
+public class aizu8_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
