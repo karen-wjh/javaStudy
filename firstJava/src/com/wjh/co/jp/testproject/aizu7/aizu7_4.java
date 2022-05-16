262fe9a775e6d3e9b255a3b6b8d5d6e5247dde64
karen-wjh <wangjinghan0725@icloud.com>
2022-05-16 14:29:51 +0900

    aizu7

---
+++ firstJava/src/com/wjh/co/jp/testproject/aizu7/aizu7_4.java
@@ -0,0 +1,25 @@
+package com.wjh.co.jp.testproject.aizu7;
+import java.util.Scanner;
+public class aizu7_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int list1 [][] = new int [a][b];
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                list1[i][j] = sc.nextInt();
+            }
+        }
+        int list2 [][] = new int [b][c];
+        for (int i = 0; i < b; i++) {
+            for (int j = 0; j < c; j++) {
+                list2[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < list2.length; i++) {
+            
+        }
+    }
+}
